81d6ef2938a1281fb471b5e167936b17483082c7
FAIDINE <mohamed.faidine@live.fr>
2021-09-21 20:57:55 +0200

    feat(my resume project) first commit

---
+++ src/containers/ResumeManager/ResumeManager.js
@@ -0,0 +1,15 @@
+import React, {Component} from 'react';
+
+class ResumeManager extends Component {
+    render() {
+        return (
+            <main className="container-fluid">
+                <nav>Navbar</nav>
+                <section>Profil page</section>
+                <footer>Pied de page</footer>
+            </main>
+        )
+    };
+}
+
+export default ResumeManager;
